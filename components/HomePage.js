e355a52474c0c42249ce4cf633371a6582d47bf3
Raquel <cruickshankrpc@gmail.com>
2020-07-02 12:46:27 +0100

    axios

--- components/HomePage.js
+++ components/HomePage.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { ProgressPlugin } from 'webpack'
 import axios from 'axios'
 
 const HomePage = () => {
+
+  const API_KEY = '089c839eda3ed1ce04045e0b371dedeb'
 
   const [search, setSearch] = useState('')
   const [query, setQuery] = useState('')
@@ -12,7 +13,13 @@ const HomePage = () => {
     getMovies()
   }, [query])
 
-  const getMovies = 
+  const getMovies = (() => {
+    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false
+    `)
+      .then(movie => {
+        setMovies(movie.data)
+      })
+  })
 
 
   const updateSearch = e => {
@@ -43,6 +50,18 @@ const HomePage = () => {
           </button>
         </form>
       </div>
+
+      <div className="movie">
+        {movie.data.results.map((result, index) => {
+          return <div key={index}>
+            <h1>{result.title}
+            </h1>
+          </div>
+        })}
+
+      </div>
+
+
 
     </section>
   </>
