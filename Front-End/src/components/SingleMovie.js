cfc8e751b953a5f340d89ea4e50eb5a632d7bc3c
Raquel <cruickshankrpc@gmail.com>
2020-07-02 16:37:51 +0100

    PLAYER WORKS!!!!!!

--- Front-End/src/components/SingleMovie.js
+++ Front-End/src/components/SingleMovie.js
@@ -10,19 +10,20 @@ const SingleMovie = (props) => {
 
   useEffect(() => {
     const movieName = props.match.params.name
+
     axios.get(`https://api.spotify.com/v1/search?q=${movieName}soundtrack&type=playlist`,
       {
         headers: { 'Authorization': 'Bearer BQBR0qhn6sHg9wNgAagQHgmcPoja20s6y5xV6SgGXQqtow9ifNI4A8i0eLRXcsfymqKFBjRkEE9W7kPmXr8' }
       })
       .then(axiosResp => {
-        console.log(axiosResp)
-        // updateSoundtrackData(axiosResp.data)
+        console.log(axiosResp.data.playlists.items[0].id)
+        updateSoundtrackData(axiosResp.data.playlists.items[0].id)
       })
   }, [])
 
   return <section>
     <div>
-      <h1>Hello</h1>
+    <iframe src={`https://open.spotify.com/embed/playlist/${soundtrackData}`} width="300" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
     </div>
   </section>
 
