19e7d9b433a863424d0066079b9315062a77fcae
Shaikh Qayum <shaikhqayum@outlook.com>
2020-07-02 12:27:42 +0100

    changes

---
+++ components/spotify.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react"
+import { Switch, Route, Link, HashRouter } from 'react-router-dom'
+
+const Search = (props) => {
+  const [query, setQuery] = useState([])
+
+  function getPlaylist(query) {
+
+  }
+
+
+
+}
