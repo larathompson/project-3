dca8b102155caca91a4745ab85764a9bf4bb706c
Raquel <cruickshankrpc@gmail.com>
2020-07-02 12:08:15 +0100

    homepage.js

---
+++ components/HomePage.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { ProgressPlugin } from 'webpack'
+import axios from 'axios'
+
+const HomePage = () => {
+
+  const [search, setSearch] = useState('')
+  const [query, setQuery] = useState('')
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    getMovies()
+  }, [query])
+
+  const getMovies = 
+
+
+  const updateSearch = e => {
+    setSearch(e.target.value)
+    console.log(search)
+  }
+
+  const getSearch = e => {
+    e.preventDefault()
+    setQuery(search)
+    // 
+    setSearch('')
+  }
+
+
+  return <>
+    <section className="home-container">
+      <div className="hero-body">
+        <form onSubmit={getSearch} className="search-form">
+          <input
+            className="search-bar"
+            type="text"
+            value={search}
+            onChange={updateSearch}
+          />
+          <button className="search-button" type="submit">
+            Search
+          </button>
+        </form>
+      </div>
+
+    </section>
+  </>
+
+}
+
+export default HomePage
