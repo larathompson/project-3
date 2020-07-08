c5adc01270690ef1513e183c5a0baa0393693c4e
Raquel <cruickshankrpc@gmail.com>
2020-07-08 18:20:05 +0100

    navbar, homepage

--- Front-End/src/components/NavBar.js
+++ Front-End/src/components/NavBar.js
@@ -9,56 +9,56 @@ const NavBar = (props) => {
     props.history.push('/')
   }
 
-  return <nav className="navbar" role="navigation" aria-label="main navigation">
+  return <nav className="navbar-container" role="navigation" aria-label="main navigation">
     <div className="navbar-menu">
-      <div className="navbar-end">
-        <div className="navbar-item">
-          <div className="buttons">
-            <div className="navbar-buttons-left">
+      {/* <div className="navbar-end">
+        <div className="navbar-item"> */}
+      <div className="buttons">
+      <div className="navbar-buttons-left">
 
-              <Link to="/" className="button is-light">
-                Home
+        <Link to="/" className="button is-light">
+          Home
               </Link>
 
-              <Link to="/latest" className="button is-Light">
-                Latest
+        <Link to="/latest" className="button is-Light">
+          Latest
               </Link>
 
-              <Link to="/women" className="button is-Light">
-                Women
+        <Link to="/women" className="button is-Light">
+          Women
               </Link>
 
-              {isLoggedIn() && <Link to="/profile" className="button is-light">
-                Favourites
+        {isLoggedIn() && <Link to="/profile" className="button is-light">
+          Favourites
               </Link>}
 
-            </div>
+      </div>
 
-            <h1>THE SCORE</h1>
+      {/* <h1>THE SCORE</h1> */}
 
-            {isLoggedIn() && <Link to='/reviews' className= "button is-Light">
-              Reviews
+      {isLoggedIn() && <Link to='/reviews' className="button is-Light">
+        Reviews
             </Link>}
 
-    
-            <div className="navbar-buttons-right">
 
-              {!isLoggedIn() && <Link to="/register" className="button is-light">
-                Register
+      <div className="navbar-buttons-right">
+
+        {!isLoggedIn() && <Link to="/register" className="button is-light">
+          Register
               </Link>}
 
-              {!isLoggedIn() && <Link to="/login" className="button is-light">
-                Login
+        {!isLoggedIn() && <Link to="/login" className="button is-light">
+          Login
               </Link>}
 
-              {isLoggedIn() && <button onClick={handleLogout} className="logout-button">
-                Logout
+        {isLoggedIn() && <button onClick={handleLogout} className="logout-button">
+          Logout
               </button>}
-            </div>
-
-          </div>
-        </div>
       </div>
+
+    </div>
+    {/* </div>
+      </div> */}
     </div>
   </nav>
 }
