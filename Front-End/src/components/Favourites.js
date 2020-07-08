342dde502b146fd54c441aa21c5e57dce5655863
akirakianna <kianna.akiralove@gmail.com>
2020-07-08 16:30:36 +0100

    styling update

--- Front-End/src/components/Favourites.js
+++ Front-End/src/components/Favourites.js
@@ -38,7 +38,7 @@ const Favourites = () => {
   }
 
   return <section className="favouritesPage">
-    <h1 className="favouritePageTitle">Favourites</h1>
+    <h1 className="favouritePageTitle">FAVOURITES</h1>
     {/* //! first time page loads, user info undefined - so need to make sure it exists. */}
     <div className="favouritePageContainer">{userInfo && userInfo.favouriteMovies.map((movie, index) => {
       return <div key={index} className="favouriteCardContainer">
@@ -46,10 +46,13 @@ const Favourites = () => {
           <div className="favouriteFront">
             <Link to={`/movie/${movie.title}/${movie.filmId}`}> <img className="favouriteImage" src={movie.poster} />
             </Link>
+            <button className="favouriteDeleteButtonMobile" value={movie.filmId} onClick={deleteFavourite}>Delete</button>
           </div>
           <div className="favouriteBack">
             <div className="favourite-back-content favouriteMiddle">
               <h1 className="favouriteMovieTitle">{movie.title}</h1>
+              {/* <Link to={`/movie/${movie.title}/${movie.filmId}`}> <img className="favouriteImageBack" src={movie.poster} />
+            </Link> */}
               <button className="favouriteDeleteButton" value={movie.filmId} onClick={deleteFavourite}>Delete</button>
             </div>
           </div>
